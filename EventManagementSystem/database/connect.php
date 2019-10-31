792591ae827edebed79d9b6f7b29262f75bac804
Chin Hui <chhew96@gmail.com>
2019-10-31 23:11:35 +0800

    Create connect.php

---
+++ EventManagementSystem/database/connect.php
@@ -0,0 +1,14 @@
+﻿<?php
+$servername = "localhost";
+$username = "root";
+$password = FALSE;
+$dbname = "ems";
+
+//Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+//Check connection
+if(!$conn){
+	die("Connection failed: ".mysqli_connect_error);
+}
+?>
